a72d7a62e7ced503ebadd868607f95b1a0d100d4
limon <moontaser009@gmail.com>
2023-08-08 19:48:09 +0600

    bubble sort optimizer added

--- sort/bubble_sort_optimize solution.cpp
+++ sort/bubble_sort_optimize solution.cpp
@@ -8,14 +8,12 @@ void bubbleSort(int array[], int size) {
 
   // loop to access each array element
   for (int step = 0; step < size-1; step++) {
+      //use flag to check if swap function is used
+      int flag=0;
       
     // loop to compare array elements
-  //normal condition
 
-    // for (int i = 0; i < size-1; i++)
-
-    //updated condition
-     for (int i = 0; i < size-1-step; i++) {
+    for (int i = 0; i < size-1; i++) {
 
       // compare two adjacent elements
       // change > to < to sort in descending order
@@ -26,8 +24,17 @@ void bubbleSort(int array[], int size) {
         int temp = array[i];
         array[i] = array[i + 1];
         array[i + 1] = temp;
+        //if swap function occured flag changes  to  1
+        flag=1;
       }
     }
+    
+    /* after swapping value check swap function is called?
+     if function called flag==1 or function not called flag==0 and
+     1st loop break */
+    if(flag==0)
+        break;
+    
   }
 }
 
@@ -48,5 +55,4 @@ int main() {
   bubbleSort(data, size);
   
   printArray(data, size);
-  return 0;
 }
